a360b476b4fbd55a72f79c64a6848dc0c469a5d2
Poonam-creator <jagdalepoonam99@gmail.com>
2019-11-20 17:11:30 +0000

    new

--- src/test/java/com/bdd/pagefactory/utils/RandomnumberHelper.java
+++ src/test/java/com/bdd/pagefactory/utils/RandomnumberHelper.java
@@ -1,5 +1,10 @@
 package com.bdd.pagefactory.utils;
 
+import java.util.Random;
+
 public class RandomnumberHelper {
+    public int randomNumberGenerator(int size){
+        return new Random().nextInt(size -1);
+    }
 
 }
